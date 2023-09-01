2c1bd4dc17b1107a9aecd5e092346b6790000788
Sreya Goswami <getsreya@gmail.com>
2023-09-01 11:05:38 +0530

    ex22

---
+++ 22/ex22.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+
+
+int main(){
+
+	int fd = open("testFile.txt",O_RDWR|O_CREAT,0777);
+	if(fd<0){
+		printf("Can't open file :(");
+		return 0;
+	}
+
+	char *a; int n;
+
+	if(!fork()){
+		a = "Parent Bottle Bag Kite\n";
+		n = strlen(a);
+		int w = write(fd,a,n);
+		if(w<0){
+			printf("Could not write to file\n");
+		}
+
+	}
+	else{
+		a = "Child Drink Cat Bolt\n";
+		n = strlen(a);
+		int w = write(fd,a,n);
+		if(w<0){
+			printf("Could not write to file\n");
+		}
+
+	}
+
+}
