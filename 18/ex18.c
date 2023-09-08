47b1b566b18c35f351188c978e285bd896237419
Sreya Goswami <getsreya@gmail.com>
2023-09-09 02:10:57 +0530

    ex18 updated desc

--- 18/ex18.c
+++ 18/ex18.c
@@ -1,37 +1,132 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<fcntl.h>
-#include<string.h>
-#include<stdlib.h>
+/**
+ * 
+ * Name: ex18.c
+ * Author: Sreya Goswami
+ * Description: Menu Driven Program to Implement Read/Write Lock for Records
+ * 
+ * */
 
-// Program to add 3 initial values of student entity
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-struct Student{
+struct student{
 	int id;
-	int marks;
+	char name[20];
 };
 
 int main(){
+	int m;
+	char menu[100];
+	struct student s;
+	struct flock fl;
+	int fd;
+	int r,w,n = sizeof(struct student);
+	strcpy(menu,"\n---------------\n[MENU]\n\t1. Initialize\n\t2. Read\n\t3. Write\n\t4. Exit\n");
 
-	struct Student s1, s2, s3;
-	s1.id=1; s1.marks=97; 
-	s2.id=2; s2.marks=93; 
-	s3.id=3; s3.marks=67; 
+	while(!0){
+		printf("%s",menu);
+		scanf("%d[^\n]",&m);
+		if(m==1){
+			fd = open("records",O_EXCL|O_CREAT,0777);
 
-	int fd = open("records.txt",O_RDWR|O_CREAT,0777);
-	if(fd<0) perror("Could not open records");
+			if(fd<0){
+				perror("Can't Initialize");
+				continue;
+			}
 
-	//since we are initializing
-	lseek(fd,0,SEEK_SET);
+			//1
+			s.id=1;strcpy(s.name,"Icy");
+			w = write(fd, &s, sizeof(s));
+			if(w<0) perror("Can't write\n");
 
-	int w1 = write(fd,&s1,sizeof(struct Student));
-	if(w1<0) perror("Could not write to records");
+			//2
+			s.id=2;strcpy(s.name,"Blume");
+			w = write(fd, &s, sizeof(s));
+			if(w<0) perror("Can't write\n");
 
-	write(fd,&s2,sizeof(struct Student));
-	write(fd,&s3,sizeof(struct Student));
+			//3
+			s.id=3;strcpy(s.name,"Stormie");
+			w = write(fd, &s, sizeof(s));
+			if(w<0) perror("Can't write\n");
 
-	close(fd);
+			close(fd);
+		}
+		else if(m==2){
+			fd = open("records",O_RDONLY,0777);
+			if(fd<0){
+				perror("Can't open records\n");
+				return -1;
+			}
+			int id;
+			printf("Enter ID: \n"); scanf("%d[^\n]",&id);
 
+			fl.l_type = F_RDLCK;
+			fl.l_whence = SEEK_SET;
+			fl.l_start = n*(id-1);
+			fl.l_len = n;
+			fl.l_pid = getpid();
+
+			printf("Requesting read lock\n");
+			fcntl(fd, F_SETLKW,&fl);
+			printf("Acquired read lock\n");
+
+			lseek(fd, n*(id-1), SEEK_SET);
+			read(fd, &s, n);
+			printf("ID: %d Name: %s\n",s.id,s.name);
+
+			fl.l_type = F_UNLCK;
+			printf("Enter to release lock...\n");
+			getchar();getchar();
+
+			fcntl(fd, F_SETLKW,&fl);
+			printf("Released lock\n");
+
+			close(fd);
+		}
+		else if(m==3){
+			fd = open("records",O_WRONLY,0777);
+			if(fd<0){
+				perror("Can't open records\n");
+				return -1;
+			}
+
+			int id; char nameBuffer[20];
+			printf("Enter ID: \n"); scanf("%d[^\n]",&id);
+			printf("Enter Name: \n"); scanf("%s[^\n]",nameBuffer);
+
+			s.id = id; strcpy(s.name,nameBuffer);
+
+			fl.l_type = F_WRLCK;
+			fl.l_whence = SEEK_SET;
+			fl.l_start = n*(id-1);
+			fl.l_len = n;
+			fl.l_pid = getpid();
+
+			printf("Requesting write lock\n");
+			fcntl(fd, F_SETLKW,&fl);
+			printf("Acquired write lock\n");
+
+			lseek(fd, n*(id-1), SEEK_SET);
+			w = write(fd, &s, n);
+			if(w<0){
+				perror("Can't write\n");
+				return -1;
+			}
+			printf("Write Operation Completed\n");
+
+			fl.l_type = F_UNLCK;
+			printf("Enter to release lock...\n");
+			getchar();getchar();
+
+			fcntl(fd, F_SETLKW,&fl);
+			printf("Released lock\n");
+
+			close(fd);
+		}
+		else break;
+	}	
 
 	return 0;
 }
