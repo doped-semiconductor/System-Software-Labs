a2bea31d7a64fd1c683b4ddd63ba1db1d9101c0f
Sreya Goswami <getsreya@gmail.com>
2023-09-10 12:45:51 +0530

    ex4 c

--- 4/ex4.c
+++ 4/ex4.c
@@ -1,3 +1,9 @@
+/* 
+    Name: ex4.c
+    Author: Sreya Goswami
+    Description: C Program to Open Files And Test O_EXCL flag
+*/
+
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -6,7 +12,7 @@ int main(){
 	int fd = open("./existingFile",O_RDONLY);
 	printf("File Descriptor: %d\n",fd);
 
-	int fd2 = open("./otherExistingFile",O_EXCL);
+	int fd2 = open("./otherExistingFile",O_EXCL|O_CREAT);
 	printf("File Descriptor 2: %d\n",fd2);
 
 	close(fd2);
